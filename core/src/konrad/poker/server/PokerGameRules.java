2e0230be3d9ef7c7f10e97c2a20ed6c4ee50badc
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-09-16 13:31:06 +0200

    refactor, dynamic playerId

--- core/src/konrad/poker/server/PokerGameRules.java
+++ core/src/konrad/poker/server/PokerGameRules.java
@@ -1,6 +1,7 @@
 package konrad.poker.server;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class PokerGameRules {
@@ -8,16 +9,15 @@ public class PokerGameRules {
     private List<PlayerScheme> players = new ArrayList<>();
     private static final int NUMBER_OF_COMPUTERS = 4;
     private static final int DEALER_ID = 6;
-    private static final int HUMAN_ID = 1;
+    private static final int HUMAN_ID = 3;
 
     public PokerGameRules() {
         //todo ma przechodzić od 1 - 5 pomijając numer gracza
-        for (int i = 0; i < NUMBER_OF_COMPUTERS; i++) {
-//            if (i == HUMAN_ID) {
-//                continue;
-//            }
-            //todo uncomment
-            players.add(new PlayerScheme(i + 2, PlayerType.COMPUTER, true));
+        for (int i = 1; players.size() < NUMBER_OF_COMPUTERS; i++) {
+            if (i == HUMAN_ID) {
+                continue;
+            }
+            players.add(new PlayerScheme(i, PlayerType.COMPUTER, true));
         }
         players.add(new PlayerScheme(HUMAN_ID, PlayerType.HUMAN, false));
         players.add(new PlayerScheme(DEALER_ID, PlayerType.DEALER, false));
@@ -25,11 +25,14 @@ public class PokerGameRules {
 
     public List<Command> getStartCommands() {
         List<Command> commandList = new ArrayList<>();
-        commandList.add(new Command(CommandType.DRAW, 2, HUMAN_ID));
         for (int i = 0; i < NUMBER_OF_COMPUTERS; i++) {
             commandList.add(new Command(CommandType.DRAW, 2, players.get(i).id()));
         }
+        commandList.add(new Command(CommandType.DRAW, 2, HUMAN_ID));
         commandList.add(new Command(CommandType.DRAW, 3, DEALER_ID));
+        commandList.sort(Comparator.comparingInt(Command::getPlayerId));
+        //todo przeanalizowac
+        //commandList.sort((command1, command2) -> command1.getPlayerId() - command2.getPlayerId());
         return commandList;
     }
 
